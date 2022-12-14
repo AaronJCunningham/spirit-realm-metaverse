46c527db881fb6b84fd37532446b288d7c56e4f9
AaronJCunningham <aaronjosephcunningham@gmail.com>
2022-12-13 17:36:40 -0800

    fixed no esacped bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  "react/no-unescaped-entities": false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

1697bcb5fa4565e02a7684957bf57d807fcef997
AaronJCunningham <aaronjosephcunningham@gmail.com>
2022-12-15 10:08:54 -0800

    fixing railway

--- metaverseapp/socketUtlities/socketConnection.js
+++ metaverseapp/socketUtlities/socketConnection.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io("ws://srbackend-production.up.railway.app");
+const socket = io("https://srbackend-production.up.railway.app");
 console.log("socket", socket);
 export default socket;

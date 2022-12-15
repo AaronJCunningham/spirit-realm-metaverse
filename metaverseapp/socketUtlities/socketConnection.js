b6861b0a3780fba315315cd24d443e9f5caf96d0
AaronJCunningham <aaronjosephcunningham@gmail.com>
2022-12-15 14:30:03 -0800

    render server

--- metaverseapp/socketUtlities/socketConnection.js
+++ metaverseapp/socketUtlities/socketConnection.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io("https://spirit-realm-backend.herokuapp.com/:8080");
+const socket = io("ws://sr-backend.onrender.com:8080");
 console.log("socket", socket);
 export default socket;

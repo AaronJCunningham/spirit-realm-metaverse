027f31a68888b29a330c412e4a814929a4a54e6a
AaronJCunningham <aaronjosephcunningham@gmail.com>
2022-12-18 17:06:56 -0800

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const config = {
+  siteUrl: "https://spiritrealm.art/",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  // ...other options
+};
+
+module.exports = config;

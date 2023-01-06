e0e269a5a9132eb4277fc9894b99687dc1815b33
AaronJCunningham <aaronjosephcunningham@gmail.com>
2023-01-06 11:40:06 -0800

    mobile 4 stats

--- metaverseapp/SpiritRealm.jsx
+++ metaverseapp/SpiritRealm.jsx
@@ -43,7 +43,7 @@ function Game({ isMobile }) {
         <Ghost rotation={[0, -Math.PI, 0]} />
       </PlayerController>
       <CameraController />
-      {/* <Stats /> */}
+      {isMobile && <Stats />}
       <Space />
       <ambientLight intensity={0.4} />
       <hemisphereLight intensity={1.2} color="#eacb6e" groundColor="blue" />
